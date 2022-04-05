cdcf237965663ed01d2527dde85b22f6d292e8ce
ujiprihatin <ujiprihatin9@gmail.com>
2022-04-05 12:29:50 +0700

    first commit

---
+++ Main.java
@@ -0,0 +1,29 @@
+class Main {
+  public static void main(String[] args) {
+    String nama;
+    String ttl;
+    String gender;
+    String pekerjaan;
+    String wn;
+    int tb;
+    int bb;
+
+    nama ="Uji Prihatin";
+    ttl ="Purbalingga,30 Februari 2001";
+    gender ="Wanita";
+    pekerjaan ="Mahasiswa";
+    wn ="Indonesia";
+    tb =159;
+    bb =52;
+
+    System.out.println("Nama " + nama);
+    System.out.println("Ttl " + ttl);
+    System.out.println("Gender " + gender);
+    System.out.println("Pekerjaan " + pekerjaan);
+    System.out.println("Wn " + wn);
+    System.out.println("Tinggi Badan " + tb);
+    System.out.println("Bb " + bb);
+  }
+}
+
+  
